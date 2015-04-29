7507ee778560cc520845398aa319eb84e6408f2e
melvinfonda <fondamelvin@gmail.com>
2015-04-29 10:05:43 +0700

    udah edit tampilan homepage, jangan lupa ganti env

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,87 +2,92 @@
 
 @section('content')
 <div class="container">
+<div class="jumbotron">
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">UKM dan Indag</h3>
 			<br>
-			<a href="#"><center><img src="{{ asset('/images/logo-1-ukmindag.jpg') }}" class="img-responsive" style="width:200px";></center></a>
+			<a href="#"><center><img src="{{ asset('/images/logo-1-ukmindag.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Kependudukan & Catatan Sipil</h3>
 			<br>
-			<a href="#"><center><img src="{{ asset('/images/logo-2-dukcapil.png') }}" class="img-responsive" style="width:200px";></center></a>
+			<a href="#"><center><img src="{{ asset('/images/logo-2-dukcapil.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Parkir dan Terminal</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-3-parkir.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-3-parkir.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
-	</div>
-	<br>
-	<br>
-	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">IMB dan Ijin Lokasi</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-4-imb.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-4-imb.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
+	</div>
+	<br>
+	<br>
+	<div class="row">
+		
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Koperasi</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-5-koperasi.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-5-koperasi.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Pelayanan Pajak</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-6-pajak.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-6-pajak.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
-	</div>
-	<br>
-	<br>
-	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Pengelolaan Sampah</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-7-sampah.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-7-sampah.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Pengaduan Masyarakat</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-8-aduan.jpg') }}" class="img-responsive" style="width:200px";></center></a>
-		</div>
-
-		<div class="col-md-4">
-			<h3 class="text-center">Ijin Usaha dan Sarana Perdagangan</h3>
-			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-9-ijin.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-8-aduan.jpg') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 	</div>
 	<br>
 	<br>
 	<div class="row">
-		<div class="col-md-4">
+		
+
+		<div class="col-md-3">
+			<h3 class="text-center">Ijin Usaha dan Sarana Perdagangan</h3>
+			<br>
+				<a href="#"><center><img src="{{ asset('/images/logo-9-ijin.png') }}" class="img-responsive" style="width:64px";></center></a>
+		</div>
+		<div class="col-md-3">
 			<h3 class="text-center">Ijin terkait Air</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-10-air.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-10-air.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Ijin terkait Lahan</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-11-lahan.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-11-lahan.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 
-		<div class="col-md-4">
+		<div class="col-md-3">
 			<h3 class="text-center">Ijin Usaha Terpadu</h3>
 			<br>
-				<a href="#"><center><img src="{{ asset('/images/logo-12-iut.png') }}" class="img-responsive" style="width:200px";></center></a>
+				<a href="#"><center><img src="{{ asset('/images/logo-12-iut.png') }}" class="img-responsive" style="width:64px";></center></a>
 		</div>
 	</div>
+	<br>
+	<br>
+	<div class="row">
+		
+	</div>
+</div>
 </div>
 @endsection
