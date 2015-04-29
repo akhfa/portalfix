92eac6ba165bbf6ee825b9fd0494e58dbf1af604
willy2706 <willylie2706@gmail.com>
2015-04-30 01:10:47 +0700

    update

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -1,12 +1,13 @@
 <div id ="check"></div>
 <script type="text/javascript" src="{{ asset('/js/jquery.min.js') }}"></script>
 <script type="text/javascript">
-	console.log("ASDFAS")
+	// console.log("ASDFAS")
 
 	$.ajax({
 		    type: 'get',
-		    url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/check',
+		    url: 'http://e-gov-bandung.tk/dukcapil/api/public/check/authenticated',
 		    success: function(data) {
+		    	console.log(data)
 		    	if (data != 'false') {
 		    		var url = "{{url()}}/home?id="+data;
 		    		window.location.href = url;
